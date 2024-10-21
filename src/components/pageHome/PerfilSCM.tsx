098aa551ad5b9b1a5f5f54ddf3c279567f3627a0
sebascm-dev <spanolocabo@gmail.com>
2024-10-21 23:02:42 +0200

    [+] CONFIGURACION DEL PAQUETE TYPEWRITER Y AÑADIENDOLO AL COMPONENTE PERFILSCM

--- src/components/pageHome/PerfilSCM.tsx
+++ src/components/pageHome/PerfilSCM.tsx
@@ -1,4 +1,20 @@
-import Image from "next/image"
+'use client';
+
+import Image from "next/image";
+
+import TypewriterComponent from "typewriter-effect";
+
+const typewriterOptions = {
+    strings: [
+        "Ingeniero de Software.",
+        "Estudiante de la Universidad de Huelva (UHU)",
+        "Amante del café.",
+        "Desarrollador FullStack.",
+        "Administrador de Base de Datos.",
+    ],
+    autoStart: true,
+    loop: true,
+};
 
 export default function PerfilSCM() {
     return (
@@ -13,7 +29,10 @@ export default function PerfilSCM() {
                 />
             </div>
             <h1 className="font-bold text-2xl mt-2">Sebastián Contreras</h1>
-            <p>Ingeniero de Software</p>
+            <p className="text-xs text-gray-300 opacity-45 -mt-0.5">Actualmente estudiando Ingeniería Informática.</p>
+            <p className="text-lg">
+                <TypewriterComponent options={typewriterOptions} />
+            </p>
         </section>
-    )
+    );
 }
