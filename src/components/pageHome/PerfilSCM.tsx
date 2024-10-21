47678cfa072384d9b885ea364c37d783e84cb57d
sebascm-dev <spanolocabo@gmail.com>
2024-10-22 00:07:45 +0200

    [ ! ] ARREGLANDO EL WARNING QUE ME SOLTABA LA CONSOLA

--- src/components/pageHome/PerfilSCM.tsx
+++ src/components/pageHome/PerfilSCM.tsx
@@ -8,8 +8,8 @@ export default function PerfilSCM() {
             <div className="size-16 relative">
                 <Image
                     src="/images/avatares/avatar_scm.webp"
-                    layout="fill"
-                    objectFit="cover"
+                    fill
+                    style={{ objectFit: "cover" }}
                     alt="Imagen de perfil de sebascm-dev"
                     className="rounded-full border-[2px] border-[#27272A] border-opacity-100"
                 />
