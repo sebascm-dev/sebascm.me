ba3a03d842ee7c351dc4fc4a73f0c600d9bef740
sebascm-dev <spanolocabo@gmail.com>
2024-10-24 23:10:40 +0200

    [+] AÑADIENDO EL DOCK DENTRO DEL LAYOUT DE LA PAGINA PRINCIPAL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import BackgroundWeb from "@/components/BackgroundWeb";
-import Dock from "../components/pageHome/Dock";
+import Dock from "../components/Dock";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
