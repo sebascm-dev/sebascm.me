696e59f6987d0096892c097e7c0969803539f90b
sebascm-dev <spanolocabo@gmail.com>
2024-10-24 22:40:29 +0200

    [+] CONSTRUYENDO LA PRIMERA VERSION DEL DOCK (MENU)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import BackgroundWeb from "@/components/BackgroundWeb";
+import Dock from "../components/pageHome/Dock";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <BackgroundWeb />
         {children}
+        <Dock />
       </body>
     </html>
   );
