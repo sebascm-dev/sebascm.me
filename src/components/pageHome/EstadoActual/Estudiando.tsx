490795dc88e82e59da505162c02a1ae8cd47770f
sebascm-dev <spanolocabo@gmail.com>
2024-10-28 21:49:27 +0100

    [+] CONFIGURANDO DE NUEVO LOS ESTADOS

--- src/components/pageHome/EstadoActual/Estudiando.tsx
+++ src/components/pageHome/EstadoActual/Estudiando.tsx
@@ -7,7 +7,7 @@ export default function Estudiando({ texto }: EstudiandoProps) {
         <span className="relative mt-4 inline-flex items-center gap-x-1.5 border-[1px] border-purple-900/30 py-1 px-2 rounded-md text-xs font-medium bg-purple-800/10 text-purple-300/50">
             <span className="absolute size-2 inline-block rounded-full bg-purple-400/50"></span>
             <span className="animate-ping size-2 inline-block rounded-full bg-purple-500/75"></span>
-            Estudiando {texto}
+            {texto}
         </span>
     )
 }
