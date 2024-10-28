a2fb665770f625edef020a33c731074c7816606d
sebascm-dev <spanolocabo@gmail.com>
2024-10-28 20:17:27 +0100

    [+] AGREGANDO LIMITE DE ANCHURA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 import PerfilSCM from "@/components/pageHome/PerfilSCM";
+import UltimoPost from "@/components/pageHome/UltimoPost";
 
 export default function Home() {
   return (
     <>
       <main className="
-        mx-auto bg-black bg-opacity-0
-        lg:p-4 lg:mt-28 lg:w-[]85%]
+        mx-auto max-w-6xl bg-black bg-opacity-0
+        lg:p-4 lg:mt-28 lg:w-[85%]
         md:p-4 md:mt-20 md:w-[85%]
         mt-16 w-[90%]
         ">
 
         {/* COMPONENTE SIMPLE DE MI PERFIL */}
         <PerfilSCM />
+
+        <section className="mt-32 flex justify-around gap-4">
+          <UltimoPost />
+          <UltimoPost />
+        </section>
       </main>
     </>
   );
