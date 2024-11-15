314f9bddbbc9e5786dc529a48a52438b83573939
sebascm-dev <spanolocabo@gmail.com>
2024-11-15 13:59:44 +0100

    [+] EMPEZANDO LA CREACION LA PAGINA DE PROYECTOS

---
+++ src/app/proyectos/page.tsx
@@ -0,0 +1,14 @@
+export default function proyectos() {
+    return (
+        <main className="
+        mx-auto max-w-6xl bg-black bg-opacity-0
+        lg:p-4 lg:mt-28 lg:w-[85%]
+        md:p-4 md:mt-20 md:w-[85%]
+        mt-16 w-[90%]
+        ">
+            <h1 className="text-3xl md:text-5xl text-pretty font-semibold text-white">Proyectos realizados durante mi carrera.</h1>
+            <p className="text-sm md:text-base mt-3 text-pretty md:mt-6 text-gray-100/75">¿No sabes por dónde empezar? He trabajado en montones de pequeños y grandes proyectos a lo largo de los años, pero estos son los que más me enorgullecen. Muchos de ellos son privados y no podré mostrarlos a continuación.</p>
+
+        </main>
+    )
+}
