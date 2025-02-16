947387a9c98c28f43fafbbbcd686f666e16f5d5f
sebascm-dev <spanolocabo@gmail.com>
2025-02-16 19:34:19 +0100

    [+] AGREGANDO MI EQUIPAMIENTO

--- src/app/padelstats/page.tsx
+++ src/app/padelstats/page.tsx
@@ -15,11 +15,45 @@ export default function padelstats() {
             <section className="flex flex-col lg:flex-row gap-4">
                 <div className="wfull lg:w-1/2">
                     <FetchPadelStatsMatch />
+
+                    <div className="mt-8 hidden lg:grid grid-cols-2 gap-4">
+                        <article className="relative border border-[#2E2D2D] rounded-md p-4 bg-[#1C1C1C]/50 shadow-lg backdrop-blur-[2px] h-fit hover:border-[#EDEDED]/30 transition-colors duration-300">
+                            <p className="mb-2 text-sm text-gray-100/50 border-l-2 border-white/70 px-1.5 h-5">NOX ML10 Limited Edition</p>
+                            <div className="flex justify-center items-center">
+                                <img src="/images/stack-padel/pala.webp" className="h-44 object-cover" alt="pala de padel" />
+                            </div>
+                        </article>
+                        <article className="relative border border-[#2E2D2D] rounded-md p-4 bg-[#1C1C1C]/50 shadow-lg backdrop-blur-[2px] h-fit hover:border-[#EDEDED]/30 transition-colors duration-300">
+                            <p className="mb-2 text-sm text-gray-100/50 border-l-2 border-white/70 px-1.5 h-5">NOX AT10 Competition XL</p>
+                            <div className="flex justify-center items-center">
+                                <img src="/images/stack-padel/paletero.webp" className="h-44 object-cover -z-20" alt="pala de padel" />
+                            </div>
+                        </article>
+
+                    </div>
                 </div>
+
+
                 <div className="wfull lg:w-1/2">
                     <FetchPadelSetsChart />
                     <FetchPadelResultsMatch />
+
+                    <div className="mt-8 lg:hidden grid grid-cols-2 gap-4">
+                        <article className="relative border border-[#2E2D2D] rounded-md p-4 bg-[#1C1C1C]/50 shadow-lg backdrop-blur-[2px] h-fit hover:border-[#EDEDED]/30 transition-colors duration-300">
+                            <p className="mb-2 text-sm text-gray-100/50 border-l-2 border-white/70 px-1.5 h-5">NOX ML10 Limited Edition</p>
+                            <div className="flex justify-center items-center">
+                                <img src="/images/stack-padel/pala.webp" className="h-44 object-cover" alt="pala de padel" />
+                            </div>
+                        </article>
+                        <article className="relative border border-[#2E2D2D] rounded-md p-4 bg-[#1C1C1C]/50 shadow-lg backdrop-blur-[2px] h-fit hover:border-[#EDEDED]/30 transition-colors duration-300">
+                            <p className="mb-2 text-sm text-gray-100/50 border-l-2 border-white/70 px-1.5 h-5">NOX AT10 Competition XL</p>
+                            <div className="flex justify-center items-center">
+                                <img src="/images/stack-padel/paletero.webp" className="h-44 object-cover -z-20" alt="pala de padel" />
+                            </div>
+                        </article>
+                    </div>
                 </div>
+
             </section>
         </main>
     )
