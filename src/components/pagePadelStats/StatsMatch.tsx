92e9520698b7bc3cc5787a28f56dffb85ef4258e
sebascm-dev <spanolocabo@gmail.com>
2025-09-30 23:25:10 +0200

    [+] AJUSTANDO EL ESPACIADO EN EL COMPONENTE StatsMatch PARA MEJORAR LA VISUALIZACIÓN DE ESTADÍSTICAS

--- src/components/pagePadelStats/StatsMatch.tsx
+++ src/components/pagePadelStats/StatsMatch.tsx
@@ -103,7 +103,7 @@ export default function StatsWidget({ matchpadel = [] }: { matchpadel: MatchPade
         <p className="mt-0.5 font-bold text-sm text-white">Estadísticas Generales</p>
       </header>
 
-      <article className="flex flex-col gap-6 flex-1 justify-center">
+      <article className="flex flex-col gap-2 flex-1 justify-center">
         {/* Estadísticas principales */}
         <div className="grid grid-cols-3 gap-4">
           {/* Totales: ganados/totales */}
